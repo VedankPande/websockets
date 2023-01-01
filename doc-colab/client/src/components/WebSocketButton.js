a8a45a7e3be8b60a0fa7a63d0222dfdc24cff472
Vedank Pande <vedank.pande@gmail.com>
2023-01-02 01:29:53 +0530

    live text updates working

--- doc-colab/client/src/components/WebSocketButton.js
+++ doc-colab/client/src/components/WebSocketButton.js
@@ -2,46 +2,41 @@ import { useEffect, useState } from "react";
 import { Row, Col, Container } from "react-bootstrap";
 import { w3cwebsocket as W3CWebSocket } from "websocket";
 
-
+const client = new W3CWebSocket('ws://127.0.0.1:8000');
+const UPDATE = "update"
 
 function WebSocketButton(){
 
-    const [data,setData] = useState("random text");
+    const [data,setData] = useState("");
+    
 
     useEffect(()=>{
-        console.log('rendered button');
+        client.onopen = () => {
+            console.log('connection opened') 
+
+            client.onmessage = (message) => {
+                const jsonMessage = JSON.parse(message.data)
+                if (jsonMessage.type === UPDATE){
+                    console.log("updating state using updated text")
+                    setData(jsonMessage.data)
+                }
+                console.log(jsonMessage);
+            }           
+        }
+        
     },[])
    
     const onTextChange = (text) =>{
+        client.send(JSON.stringify({type:"update",data: text.target.value}))
+        setData(text.target.value)
         console.log(text.target.value);
     }
-    const connect = () =>{
-        const client = new W3CWebSocket('ws://127.0.0.1:8000');
-        client.onopen = () =>{
-            console.log('connection opened');
-            try{
-                client.send(JSON.stringify({message: 'hello server'}))
-            }
-            catch(e){
-                console.log(e);
-            }
-    
-        }
-        
-        client.onmessage = (message) => {
-            console.log(message.data);
-        }
-    
-    }
+
 
     return(
         <div>
-            <button onClick={connect}>
-            Click to connect to wsServer
-            </button>
-            <br></br>
-            <textarea onChange={onTextChange}>{data}</textarea>
-        </div>
+            <textarea value = {data}onChange={onTextChange} placeholder={"start typing here..."}></textarea>
+        </div> 
     )
 }
 
--- doc-colab/server/index.js
+++ doc-colab/server/index.js
@@ -1,6 +1,8 @@
 const wss = require('ws');
 const http = require('http');
 
+const UPDATE = "update"
+
 server = new wss.WebSocketServer({port: 8000})
 console.log('created websocket server')
 
@@ -8,16 +10,25 @@ console.log('created websocket server')
 server.on('connection',function(connection, req){
     console.log("A new device connected");
 
+    server.clients.forEach(client => {
+        console.log("num clients:",server.clients.size)
+        if (client !== connection && client.readyState === wss.WebSocket.OPEN){
+            client.send(JSON.stringify({type:"notif",data:"A new device joined the document"}));
+        }            
+    }); 
+
     connection.on('message',function(message){
         
-        console.log(message.toString())
-        connection.send('hello from the server!')
-        console.log("num clients:", server.clients.size)
-        server.clients.forEach(client => {
-            if (client !== connection && client.readyState === wss.WebSocket.OPEN){
-                console.log('sending broadcast')
-                client.send('A new device joined the document');
-            }            
-        });
+        const data = JSON.parse(message)
+        if (data.type === UPDATE){
+            console.log("updates from editor:", data.data)
+
+            server.clients.forEach(client => {
+                if (client !== connection && client.readyState === wss.WebSocket.OPEN){
+                    client.send(JSON.stringify({type:"update",data:data.data}));
+                }            
+            });
+
+        }        
     })
 })
